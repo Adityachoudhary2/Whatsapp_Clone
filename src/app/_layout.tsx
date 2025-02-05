547bf257c65a7dfc0ceed90c9703af903e9c4e7d
Aditya <choudharyaditya9381@gmail.com>
2025-02-05 17:19:37 +0530

    same

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,30 +1,21 @@
-import { Stack } from 'expo-router'
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
 //auto hiding of the splash screen
 SplashScreen.preventAutoHideAsync();
 
-const RootNavigation: React.FC = () => {
-    const RootLayout: React.FC = () => {
-        useEffect(() => {
-            
-            SplashScreen.hideAsync();
-       
-    })
-    
-        return null; // You might want to render something here.
-      };
-    
-    return (
 
-        <Stack>
-            <Stack.Screen name='index'/>
-        </Stack>
+  const RootLayout: React.FC = () => {
+    useEffect(() => {
+      SplashScreen.hideAsync();
+    },[]);
 
-    )
-}
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+};
 
-export default RootNavigation
-
+export default RootLayout;
