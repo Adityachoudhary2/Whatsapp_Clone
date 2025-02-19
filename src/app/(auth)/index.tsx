5959531f839ad37ad8b59ea12600a10e9dce19f7
Aditya <choudharyaditya9381@gmail.com>
2025-02-19 16:17:15 +0530

    include loading screen and redirect login welcome page

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -1,21 +1,49 @@
 import imagePath from '@/src/constants/imagePath';
-import {Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { router } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { verticalScale, moderateScale } from 'react-native-size-matters';
 
 const Auth: React.FC = () => {
+
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setIsLoading(true)
+    }, 2000);
+
+    const navigationTimeout = setTimeout(() => {
+      router.push('/(auth)/terms_agree')
+    }, 3000);
+
+  }, [])
+
+
   return (
     <SafeAreaView style={styles.container}>
-    <View style={styles.header}></View>
-    <View style={styles.body}>
-      <Image source={imagePath.logo} style={styles.logo_style} resizeMode='contain' />
-      <Text style={styles.whatsapp_text}>WhatsApp</Text>
-    </View>
-    <View style={styles.footer}>
-      <Text style={styles.from_text}>From</Text>
-      <Text style={styles.facebook_text}>Facebook</Text>
-    </View>
-  </SafeAreaView>
-   );
+      <View style={styles.header}></View>
+      <View style={styles.body}>
+        <Image source={imagePath.logo} style={styles.logo_style} resizeMode='contain' />
+        <Text style={styles.whatsapp_text}>WhatsApp</Text>
+      </View>
+      <View style={styles.footer}>
+
+        {isLoading ? (
+          <>
+            <ActivityIndicator size={moderateScale(40)} color={"#0CCC83"} />
+            <Text style={styles.loading_text}>Loading...</Text>
+          </>
+        ) : (
+          <>
+            <Text style={styles.from_text}>From</Text>
+            <Text style={styles.facebook_text}>Facebook</Text>
+          </>
+        )}
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Auth;
@@ -35,6 +63,8 @@ const styles = StyleSheet.create({
   },
   footer: {
     alignItems: 'center',
+     height: verticalScale(80),
+    justifyContent: "flex-end"
   },
   from_text: {
     fontSize: moderateScale(12),
@@ -53,4 +83,10 @@ const styles = StyleSheet.create({
     fontSize: moderateScale(35),
     color: '#000000',
   },
+  loading_text: {
+    fontSize: moderateScale(20),
+    color: "#00A884",
+    fontWeight: "bold",
+    marginTop: verticalScale(10),
+  }
 });
